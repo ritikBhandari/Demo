c6524f2add4b9c4cc506305b155dd8e874d6e2b9
ritikBhandari <ritikbhandari68@gmail.com>
2023-03-08 02:55:48 +0530

    created sample code

---
+++ demo.py
@@ -0,0 +1,9 @@
+while (True) :
+    # f.write(f"{sys.argv[1]} {sys.argv[2]}\n")
+    # i=i+1
+    # f.flush()
+    # f.seek(0)
+    # print(f.read())
+    # time.sleep(2)
+    print(f"{sys.argv[1]}")
+    time.sleep(2)
